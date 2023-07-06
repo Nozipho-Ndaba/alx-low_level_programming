d6c028d9d64bb6079440119c96a15293ae4ce71e
Nozipho-Ndaba <Ntombikayisenhlane001@gmail.com>
2023-07-06 15:33:08 -0700

    my phone

--- 0x05-pointers_arrays_strings/0-reset_to_98.c
+++ 0x05-pointers_arrays_strings/0-reset_to_98.c
@@ -1,12 +1,12 @@
-#include "main.h"
+#iinclude "main.h"
 
 /**
- *  * reset_to_98 - function declaration
- *   * @n: pointer interger
- *    * Return: void
- *     */
+ *reset_to_98 - function declaration
+ *@n: pointer interger
+ *Return: void
+ */
 
 void reset_to_98(int *n)
 {
 		*n = 98;
-}
+
