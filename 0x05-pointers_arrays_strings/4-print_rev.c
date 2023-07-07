848090d48a95e537001da97a07abcaf6e0755cda
Nozipho-Ndaba <Ntombikayisenhlane001@gmail.com>
2023-07-07 04:32:52 -0700

    my phone

--- 0x05-pointers_arrays_strings/4-print_rev.c
+++ 0x05-pointers_arrays_strings/4-print_rev.c
@@ -1,24 +1,24 @@
 #include "main.h"
 
 /**
- *  * print_rev - prints a string in reverse
- *   * @s: input string.
- *    *
- *     * Return: void
- *      */
+ * print_rev - prints a string in reverse
+ * @s: input string.
+ *
+ * Return: void
+ */
 
 void print_rev(char *s)
 {
-		int count = 0;
+	int count = 0;
 
-			while (count >= 0)
-					{
-								if (s[count] == '\0')
-												break;
-										count++;
-											}
+	while (count >= 0)
+	{
+		if (s[count] == '\0')
+			break;
+		count++;
+	}
 
-				for (count--; count >= 0; count--)
-							_putchar(s[count]);
-					_putchar('\n');
+	for (count--; count >= 0; count--)
+		_putchar(s[count]);
+	_putchar('\n');
 }
