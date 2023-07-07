b28c13136ea12b23a52d1044618d3c85734a030c
Nozipho-Ndaba <Ntombikayisenhlane001@gmail.com>
2023-07-07 13:40:05 -0700

    delight

--- 0x05-pointers_arrays_strings/6-puts2.c
+++ 0x05-pointers_arrays_strings/6-puts2.c
@@ -1,15 +1,17 @@
 #include "main.h"
 #include <stdio.h>
 /**
- *   puts2 - Prints every other character of a string
- *   @str: A pointer
- *    Return: 0
+ * puts2 - Prints every other character of a string
+ * @str: A pointer
+ * Return: 0
  */
 void puts2(char *str)
-{ 	int len = 0;
+{
+	int len = 0;
 	int z = 0;
 	char *y = str;
 	int i;
+
 	while (*y != '\0')
 	{
 		y++;
@@ -20,7 +22,8 @@ void puts2(char *str)
 	{
 		if (i % 2 == 0)
 		{
-			putchar(str[i]);						}
+			putchar(str[i]);
+		}
 	}
 	putchar('\n');
 }
